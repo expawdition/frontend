9482bd29a2e984ce614ce4b042e429fb14f7db51
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-05-20 17:51:55 -0700

    trip card skeleton

---
+++ components/TripCard.tsx
@@ -0,0 +1,8 @@
+import styles from "@/styles/Home.module.css";
+export default function TripCard() {
+	return (
+		<>
+			<div className={styles.tripcard}>this will be a trip card</div>
+		</>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Text, Stack, Flex } from "@chakra-ui/react";
+import { Heading, Text, Stack, Flex, Image } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import TripCard from "@/components/TripCard";
+import Logo from "frontend/images/cmd-f.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +20,15 @@ export default function Home() {
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+			<div className={styles.header}>
+				<Image src="frontend/images/cmd-f" alt="cmd-f" />
+			</div>
 			<main className={`${styles.main} ${inter.className}`}>
 				<div className={styles.grid}>
 					<Flex direction="column" minWidth="max-content">
-						<Text fontSize="6xl">Hello, Name!</Text>
+						<Heading as="h1" size="4xl" noOfLines={1}>
+							Hello, Name!
+						</Heading>
 						<Tabs variant="soft-rounded" colorScheme="blue">
 							<TabList>
 								<Tab>Upcoming Plans</Tab>
@@ -30,7 +36,7 @@ export default function Home() {
 							</TabList>
 							<TabPanels>
 								<TabPanel>
-									<p>add component here</p>
+									<TripCard></TripCard>
 								</TabPanel>
 								<TabPanel>
 									<p>add other component here</p>
